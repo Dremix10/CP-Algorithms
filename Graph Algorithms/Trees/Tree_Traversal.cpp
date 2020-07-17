367c2a8f0d4befd5debfca5f19e2ca3bc22ecc67
Demetris Chrysostomou <demetrischrysostomou1@gmail.com>
2020-07-17 17:29:51 +0300

    Added Tree Traversal Implementation

--- Graph Algorithms/Trees/Tree_Traversal.cpp
+++ Graph Algorithms/Trees/Tree_Traversal.cpp
@@ -1,11 +1,32 @@
 #include<bits/stdc++.h>
+#define N (int)(1e5+1)
 using namespace std;
 
+vector<vector<int> > adj(N,vector<int>());
+
+void dfs(int s, int p){
+    cout<<s<<" ";
+    for(auto x : adj[s])
+        if(x!=p)
+        dfs(x,s);
+}
 
 int main (){
+    int n;
+    cin>>n;
 
+    int i;
+    int root;
+    cin>>root;
+    
+    for(i=0;i<n-1;i++){
+        int x,y;
+        cin>>x>>y;
+        adj[x].push_back(y);
+        adj[y].push_back(x);
+    }
 
-    cout<<0<<endl;
-    cout<<"testing 123"<<endl;
-    return 0;
+    cout<<"Nodes in DFS order are:"<<endl;
+    dfs(root,root);
+    
 }
