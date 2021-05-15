72d87b6f4b7a89bf8fe294bd874aad1b1471baa0
Demetris Chrysostomou <demetrischrysostomou1@gmail.com>
2021-05-15 13:12:17 +0300

    Fixed several bugs in hashing.cpp

--- String Algorithms/Hashing.cpp
+++ String Algorithms/Hashing.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef unsigned long long ll;
+typedef long long ll;
+typedef unsigned long long ull;
 const int N = 2e6+1;
 
 // randomization setup
@@ -8,15 +9,16 @@ mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 uniform_int_distribution<ll> distribution;
 ll rnd(ll l, ll r){return distribution(rng)%(r-l+1)+l;}
 
-const ll base = 31;
+const ll base = 256;
+const int M = 3;
 
-void slide(ll &hash, ll leftPow, char rem, char add, ll mod){
-    hash = (hash - (rem-'a'+1)*leftPow + mod)%mod;
-    hash = (hash*base + add-'a'+1)%mod;
+void slide(ll &hash, ll leftPow, ll rem, ll add, ll mod){
+    hash = (hash + mod - ((rem*leftPow)%mod))%mod;
+    hash = (hash*base + add)%mod;
 }
 
-bool comp(ll x[], ll y[], int n){
-    for(int i = 0;i<n;i++){
+bool comp(ll x[], ll y[]){
+    for(int i = 0;i<M;i++){
         if(x[i]!=y[i])return false;
     }
     return true;
@@ -37,31 +39,38 @@ int main (){
         return 0;
     }
 
-    ll leftPow[3]={1,1,1},Htxt[3]={},Hpat[3]={};
-    ll mod[3] = {(ll)1e9+7,998244353,rnd(1e8,1e9)};
+    ll leftPow[M]={1,1,1},Htxt[M]={},Hpat[M]={};
+    ll mod[M] = {(ll)1e9+9,(ll)1e9+7,998244353};
+    ull leftPowU = 1,HtxtU = 0,HpatU = 0;
 
     int i,j;
 
     for(i=0;i<m;i++){
         if(i){
-            for(j=0;j<3;j++)
+            for(j=0;j<M;j++)
                 leftPow[j] = (leftPow[j]*base)%mod[j];
+            leftPowU = leftPowU*base;
         }
-        for(j=0;j<3;j++){
+        for(j=0;j<M;j++){
             Hpat[j] = (Hpat[j] * base)%mod[j];
-            Hpat[j] = (Hpat[j] + pat[i]-'a'+1)%mod[j];
+            Hpat[j] = (Hpat[j] + (int)(pat[i]))%mod[j];
             Htxt[j] = (Htxt[j] * base)%mod[j];
-            Htxt[j] = (Htxt[j] + txt[i]-'a'+1)%mod[j];
+            Htxt[j] = (Htxt[j] + (int)(txt[i]))%mod[j];
         }
+        HpatU = HpatU * base + (int)(pat[i]);
+        HtxtU = HtxtU * base + (int)(txt[i]);
     }
 
     int ans = 0;
-    if(comp(Htxt,Hpat,3))ans++;
+    if(comp(Htxt,Hpat) && HpatU == HtxtU)ans++;
 
     for(i=m;i<n;i++){
-        for(j=0;j<3;j++)
+        for(j=0;j<M;j++){
             slide(Htxt[j],leftPow[j],txt[i-m],txt[i],mod[j]);
-        if(comp(Htxt,Hpat,3))ans++;
+        }
+        HtxtU = HtxtU - (int)(txt[i-m])*leftPowU;
+        HtxtU = HtxtU * base + (int)(txt[i]);
+        if(comp(Htxt,Hpat) && HpatU == HtxtU)ans++;
     }
     cout<<ans<<endl;
 
